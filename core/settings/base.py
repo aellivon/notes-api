8ac46c435f418fe922ceb93387ae591eb5975052
Aellivon Kyle Sun <aellivon@gmail.com>
2023-02-15 21:39:30 +0900

    Add default settings for postgres

--- core/settings/base.py
+++ core/settings/base.py
@@ -95,10 +95,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DATABASE_NAME'),
-        'USER': config('DATABASE_USER'),
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        'HOST': config('DATABASE_HOST'),
+        'NAME': config('DATABASE_NAME', default="project_ci_test"),
+        'USER': config('DATABASE_USER', default="postgres"),
+        'PASSWORD': config('DATABASE_PASSWORD', default=""),
+        'HOST': config('DATABASE_HOST', default="postgres"),
     }
 }
 
